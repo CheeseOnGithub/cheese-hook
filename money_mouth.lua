2a9b92b13286b3aca31c44ab5f7a3f2377a5ecf8
CheeseOnGithub <gamingman32@protonmail.com>
2023-07-05 07:34:33 +0200

    aaaa

--- money_mouth.lua
+++ money_mouth.lua
@@ -1,75 +1,148 @@
-local Title = "MikeCash - Please visit the website."
-
-local API_HOST = "s1.wayauth.com" -- s1.wayauth.com, s2.wayauth.com, s3.wayauth.com, s4.wayauth.com, s5.wayauth.com
-local LINKVERTISE_ID = 481016 -- Change me
-local LINKVERTISE_COUNT = 2 -- Change me
-local TOKEN_EXPIRE_TIME = 7200 -- Seconds -- 2 hours in seconds :3
-
-local Task = {}
-Task.__index = Task
-
-local HttpService = game:GetService("HttpService")
-local SHA2 = loadstring(game:HttpGet("https://raw.githubusercontent.com/Egor-Skriptunoff/pure_lua_SHA/master/sha2.lua"))()
 local Iris = loadstring(game:HttpGet("https://raw.githubusercontent.com/x0581/Iris-Exploit-Bundle/main/bundle.lua"))().Init(game.CoreGui)
+local MD5 = loadstring(game:HttpGet("https://raw.githubusercontent.com/kikito/md5.lua/master/md5.lua"))()
 
-function Task.new(API_HOST, LinkvertiseID, LinkCount, TokenExpireTime)
-    local nTask = {}
+local LINKVERTISE_ID = 481016
 
-    nTask.API_HOST = API_HOST or "s1.wayauth.com"
-    nTask.LinkvertiseID = LinkvertiseID or 12345
-    nTask.LinkCount = LinkCount or 1
-    nTask.Validator = tostring(math.random() + math.random(1, 100000) + Random.new():NextNumber())
-    nTask.TokenExpireTime = TokenExpireTime or 0
+local DONT_RENDER_CONFIRMATION_WINDOW = true
+local CREATOR_DISCORD_SERVER = "https://discord.gg/ccarnEMG95"
+local API_HOST = "s1.wayauth.com"
 
-    return setmetatable(nTask, Task)
-end
+local Solved = Iris.State(false)
+local Continue = Iris.State(DONT_RENDER_CONFIRMATION_WINDOW)
+local SupportTheCreator = Iris.State(true)
+local LearnMore = Iris.State(false)
 
-function Task:create()
-    local URLBase = "http://%s/v2/create/%s/%s/%s"
-    local URL = URLBase:format(self.API_HOST, self.LinkvertiseID, self.Validator, self.LinkCount)
-    self.task = HttpService:JSONDecode(game:HttpGet(URL))
-    return self.task
-end
+local TaskID = "NONE"
+local ExpireIn = 0
+local APIVersion = game:HttpGet(("https://%s/tasks/version"):format(API_HOST))
+local Rand = 0
 
-function Task:verify()
-    local URLBase = "http://%s/v2/verify/%s/%s/%s"
-    local URL = URLBase:format(self.API_HOST, self.task.id, self.Validator, self.TokenExpireTime)
-    local Response = HttpService:JSONDecode(game:HttpGet(URL))
-    self.data = Response
-    if Response.success then
-        if SHA2.sha256(self.Validator):upper() == Response.validator:upper() then
-            return true
+
+local function CreateTask()
+    Rand = math.random(1, 100000)
+    Rand = Rand + math.random(1, 100000)
+    Rand = Rand + math.random(1, 100000)
+    Rand = Rand + Random.new():NextNumber()
+    local Resp = game:HttpGet(("https://%s/tasks/create/%s/%s"):format(API_HOST, tostring(LINKVERTISE_ID), tostring(Rand)))
+
+    local Data = game:GetService("HttpService"):JSONDecode(Resp)
+    TaskID = Data.task_id
+    ExpireIn = Data.expire_in
+
+    task.spawn(function()
+        while task.wait(1) do
+            ExpireIn = ExpireIn - 1
         end
-    end
-    return false
+    end)
 end
 
-function Task:returnURL()
-    local URLBase = "http://%s/v2/wait/%s"
-    local URL = URLBase:format(self.API_HOST, self.task.id)
-    return URL
+if DONT_RENDER_CONFIRMATION_WINDOW then
+    CreateTask()
 end
 
-local nTask = Task.new(API_HOST, LINKVERTISE_ID, LINKVERTISE_COUNT, TOKEN_EXPIRE_TIME); nTask:create()
-local Verified = false
+local function RenderMainWindow()
+    Iris.Window({"Mike Cash - Support The Creator", [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(450, 100))}) do
+        Iris.Text({("Task ID: %s"):format(TaskID)})
+        Iris.Text({("Task Expires In: %s second(s)"):format(tostring(ExpireIn))})
+        -- Iris.Text({("API Version: %s"):format(APIVersion)})
+        -- Iris.Text({("Host: %s"):format(API_HOST)})
 
-Iris:Connect(function()
-    if not Verified then
-        Iris.Window({Title, [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(375, 60))}) do
-            Iris.SameLine() do
-                if Iris.Button({"I have visited the website."}).clicked then
-                    task.spawn(function()
-                        Verified = nTask:verify()
-                    end)
-                end
-                if Iris.Button({"Copy Website"}).clicked then
-                    nTask:returnURL()
-                end
-                Iris.End()
+        Iris.SameLine() do
+            if Iris.Button({"I have visited the Website"}).clicked then
+                task.spawn(function()
+                    local Response = game:GetService("HttpService"):JSONDecode(game:HttpGet(("https://%s/tasks/validate/%s"):format(API_HOST, TaskID)))
+                    if Response.exists and Response.solved and Response.rand_hash == MD5.sumhexa(tostring(Rand)) then
+                        Solved.value = true
+                    end
+                end)
+            end
+            if Iris.Button({"Copy Website"}).clicked then
+                setclipboard(("https://%s/tasks/begin/%s"):format(API_HOST, TaskID))
+            end
+            if Iris.Button({"Learn More"}).clicked then
+                LearnMore.value = true
             end
             Iris.End()
         end
+        Iris.End()
     end
+end
+
+local function RenderConfirmation()
+    local Loading = Iris.State(false)
+
+    Iris.Window({"Mike Cash - Support The Creator", [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(300, 75))}) do
+        if not Continue.value then
+            Iris.Checkbox({"I wish to support the creator."}, {isChecked = SupportTheCreator})
+            Iris.SameLine() do
+                if Iris.Button({Loading.value and "Please wait.." or "Continue"}).clicked then
+                    if SupportTheCreator.value then
+                        if not Loading.value then
+                            task.spawn(function()
+                                Loading.value = true
+                                CreateTask()
+                                Continue.value = true
+                            end)
+                        end
+                    else
+                        Solved.value = true
+                    end
+                end
+                if Iris.Button({"Learn More"}).clicked then
+                    LearnMore.value = true
+                end
+                Iris.End()
+            end
+        end
+        Iris.End()
+    end
+end
+
+local function RenderLearnMore()
+    Iris.Window({"Mike Cash - Support The Creator", [Iris.Args.Window.NoClose] = true, [Iris.Args.Window.NoResize] = true, [Iris.Args.Window.NoScrollbar] = true, [Iris.Args.Window.NoCollapse] = true}, {size = Iris.State(Vector2.new(600, 275))}) do
+        Iris.Text({"Mike Cash is created to let script creators easily monetize their script"})
+        Iris.Text({"without them having to pay for some sort of a license or hosting so they"})
+        Iris.Text({"can host their own \"Key System\" of some sorts."})
+        Iris.Text({""})
+        Iris.Text({"Mike Cash does not use keys, instead it uses Task IDs, which allow us"})
+        Iris.Text({"to identify if you have visited or have not visited the linkvertise yet."})
+        Iris.Text({""})
+        Iris.SameLine() do
+            Iris.Text({"Join the Creators Discord Server!"})
+            if Iris.Button({"Copy Invite"}).clicked then
+                setclipboard(CREATOR_DISCORD_SERVER)
+            end
+            Iris.End()
+        end
+        Iris.SameLine() do
+            Iris.Text({"Use MikeCash yourself!"})
+            if Iris.Button({"Copy GitHub Link"}).clicked then
+                setclipboard("https://github.com/x0581/MikeCash")
+            end
+            Iris.End()
+        end
+        Iris.Text({""})
+        if Iris.Button({"Close"}).clicked then
+            LearnMore.value = false
+        end
+        Iris.End()
+    end
+end
+
+-- Pcall because errors on finish and i aint gonna bother
+pcall(function()
+    Iris:Connect(function()
+        if not Solved.value then
+            if Continue.value then
+                RenderMainWindow()
+            else
+                RenderConfirmation()
+            end
+        end
+        if LearnMore.value then
+            RenderLearnMore()
+        end
+    end)
 end)
 
 local TextGui = Instance.new("ScreenGui", game:GetService("CoreGui"))
@@ -80,14 +153,7 @@ TextFrame.Position = UDim2.new(0.5, -TextFrame.AbsoluteSize.X / 2, 0.5, -TextFra
 local Text = Instance.new("TextBox", TextFrame)
 Text.Size = UDim2.new(1,0,1,0)
 Text.ClearTextOnFocus = false
-Text.Text = nTask:returnURL()
+Text.Text = ("https://%s/tasks/begin/%s"):format(API_HOST, TaskID)
 
-
-repeat task.wait() until Verified
-
-warn("Verified website at", nTask.data.timestamp)
-warn("Finished at", os.date("%c"))
-warn("Authentication Token", nTask.token)
-warn("Authentication Token Expire Time", TOKEN_EXPIRE_TIME)
-
+repeat task.wait() until Solved.value
 TextGui:Destroy()
